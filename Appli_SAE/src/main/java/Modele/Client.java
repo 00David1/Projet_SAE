57f9b189aba5532bf14cceb94e5ae279a1811369
00David1 <davidgrondin103@gmail.com>
2023-06-02 17:36:16 +0200

    Ajout de la classe Client

---
+++ Appli_SAE/src/main/java/Modele/Client.java
@@ -0,0 +1,15 @@
+package Modele;
+
+import java.io.File;
+import java.util.List;
+
+public class Client{
+    public static void main (String [] args){
+        File nomFichier = new File("C:\\Users\\33765\\Desktop\\Jeux_sae\\Scenario\\scenario_0.txt");
+        Scenario contenu = LectureFichierTexte.lecture(nomFichier);
+
+        SolutionEfficace solutionEf = new SolutionEfficace(contenu.quetes);
+        SolutionExhaustive solutionEx = new SolutionExhaustive(contenu.quetes);
+        solutionEf.resoudreQuetes();
+    }
+}
