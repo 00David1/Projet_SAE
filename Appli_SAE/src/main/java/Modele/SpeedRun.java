5607a7bac6727c7d3790cd32ce5fc96ce40a578d
00David1 <davidgrondin103@gmail.com>
2023-06-07 22:18:42 +0200

    Ajout due la méthode afficherCheminsAvecDuree dans SpeedRun

--- Appli_SAE/src/main/java/Modele/SpeedRun.java
+++ Appli_SAE/src/main/java/Modele/SpeedRun.java
@@ -1,6 +1,9 @@
 package modele;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SpeedRun {
     private List<List<Integer>> chemins;
@@ -10,4 +13,37 @@ public class SpeedRun {
     private int[] posActuele ;
     private List<Integer> experienceTotal;
     private int experienceRequis;
+
+    /**
+     * Constructeur de la classe CheminVersQuete0.
+     */
+    public SpeedRun(List<Quete> quetes) {
+        this.quetes = quetes;
+        this.chemins = new ArrayList<>();
+        this.durees = new ArrayList<>(); // Initialisation de la liste des durées
+        this.posActuele = new int[]{0, 0};
+        this.quetesCopy = new ArrayList<>(quetes); // Créer une copie indépendante de la liste quetes
+        this.experienceTotal = new ArrayList<>();
+        this.experienceRequis = 0;
+    }
+
+    public void afficherCheminsAvecDurees() {
+        List<List<Integer>> chemins = obtenirChemins();
+        List<Integer> durees = getDurees();
+        List<Integer> experienceA = getExperienceTotal();
+
+        Map<List<Integer>, Integer> cheminDureeMap = new HashMap<>();
+
+        for (int i = 0; i < chemins.size(); i++) {
+            List<Integer> chemin = chemins.get(i);
+            int duree = durees.get(i);
+            int XP = experienceA.get(i);
+            cheminDureeMap.put(chemin, duree);
+            //System.out.println("Chemin : " + chemin + " Durée : " + duree + " XP : " + XP);
+        }
+
+        List<Integer> cheminMinDuree = trouverCheminMinDuree(cheminDureeMap);
+        int dureeMin = cheminDureeMap.get(cheminMinDuree);
+        System.out.println("Chemin avec la durée minimale : " + cheminMinDuree + " Durée minimale : " + dureeMin);
+    }
 }
