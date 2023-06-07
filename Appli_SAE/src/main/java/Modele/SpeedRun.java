c3f149d1c8439273cd336d6b0d0340f254e57161
00David1 <davidgrondin103@gmail.com>
2023-06-07 22:15:01 +0200

    Ajout des champs de la class SpeedRun

--- Appli_SAE/src/main/java/Modele/SpeedRun.java
+++ Appli_SAE/src/main/java/Modele/SpeedRun.java
@@ -1,3 +1,13 @@
-package Modele;
+package modele;
+
+import java.util.List;
+
 public class SpeedRun {
+    private List<List<Integer>> chemins;
+    private List<Integer> durees; // Ajout de la liste des durées
+    private List<Quete> quetes;
+    private List<Quete> quetesCopy;
+    private int[] posActuele ;
+    private List<Integer> experienceTotal;
+    private int experienceRequis;
 }
