0b358eab1ffc9ad2965863a243fd9fd852d779d5
00David1 <davidgrondin103@gmail.com>
2023-06-07 22:33:05 +0200

    Ajout des méthode dans SpeedRun

--- Appli_SAE/src/main/java/Modele/Client.java
+++ Appli_SAE/src/main/java/Modele/Client.java
@@ -1,15 +1,18 @@
-package Modele;
+package modele;
 
 import java.io.File;
 import java.util.List;
 
 public class Client{
     public static void main (String [] args){
-        /*File nomFichier = new File("C:\\Users\\33765\\Desktop\\Jeux_sae\\Scenario\\scenario_0.txt");
+        File nomFichier = new File("C:\\Users\\33765\\Desktop\\Jeux_sae\\Scenario\\scenario_0.txt");
         Scenario contenu = LectureFichierTexte.lecture(nomFichier);
 
         SolutionEfficace solutionEf = new SolutionEfficace(contenu.quetes);
         SolutionExhaustive solutionEx = new SolutionExhaustive(contenu.quetes);
-        solutionEf.resoudreQuetes();*/
+        //solutionEf.resoudreQuetes();
+
+        SpeedRun cheminsVersQuete0 = new SpeedRun(contenu.quetes);
+        cheminsVersQuete0.afficherCheminsAvecDurees();
     }
 }
