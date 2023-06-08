aeaa251a30d4017332e8ba54d85b0240acd7255f
Enola <93427394+Loala04@users.noreply.github.com>
2023-06-08 09:21:48 +0200

    Mise à jour design GridPaneVide.java

--- Appli_SAE/src/main/java/Vue/GridPaneVide.java
+++ Appli_SAE/src/main/java/Vue/GridPaneVide.java
@@ -10,13 +10,14 @@ public class GridPaneVide extends GridPane {
 
     public GridPaneVide(VBoxChoix vBoxChoix) {
 
-        setPadding(new Insets(30, 0, 30, 0));
+        setPadding(new Insets(30, 0, 30, 50));
         setVgap(10);
         setHgap(10);
 
         // ------------------------------------------------SOLUTION EXHAUSTIVE ----------------------------------------------------
         //Titre
         Label titreLabel = new Label("Scénario ");
+        titreLabel.getStyleClass().add("label-gras");
         GridPane.setHalignment(titreLabel, HPos.CENTER);
         add(titreLabel, 1, 0, 5, 1);
 
@@ -27,17 +28,21 @@ public class GridPaneVide extends GridPane {
 
         // Création des éléments
         Label quetesRealiseesLabel = new Label("Quêtes Finales : " );
+        quetesRealiseesLabel.getStyleClass().add("label-gras");
         Label QuetRea = new Label(" " );
         QuetRea.setMaxSize(100, 15);
         QuetRea.setMinSize(100, 15);
 
         Label xpLabel = new Label("XP : ");
+        xpLabel.getStyleClass().add("label-gras");
 
 
         Label distanceLabel = new Label("Distance : ");
+        distanceLabel.getStyleClass().add("label-gras");
 
 
         Label dureeLabel = new Label("Durée : " );
+        dureeLabel.getStyleClass().add("label-gras");
 
 
         //Placement des éléments
@@ -52,6 +57,7 @@ public class GridPaneVide extends GridPane {
 
         //Placement tableau des quetes listées
         Label TitrequetesRealiseesPdtLabel = new Label("Quêtes réalisées pendant : ");
+        TitrequetesRealiseesPdtLabel.getStyleClass().add("label-gras");
         Label choiQuete = new Label("En attente de votre choix ... ");
 
         add(TitrequetesRealiseesPdtLabel, 5, 2);
